e3f3328185310702bebd5a604e273d1ba4341502
tonyjurg <github@jurgnet.nl>
2024-02-28 14:05:41 +0100

    updating viewtype function

--- app/app.py
+++ app/app.py
@@ -2,10 +2,8 @@ from tf.advanced.app import App
 from tf.advanced.display import displaySetup
 
 class TfApp(App):
-    def __init__(app, *args, **kwargs):
-        super().__init__(*args, **kwargs)
 
-    def Viewtype(app,viewName):
+    def viewtype(app,viewName):
         """
         Configures and displays the Text-Fabric application with specified view options.
 
@@ -31,17 +29,29 @@ class TfApp(App):
 
              note: here the A is TF's advanced API which is the implied object passed as argument 'app' to this function.
         """
-
+        feedback='Display is setup for viewtype '
         if viewName=='wg':
+           # modify some labels for sentence nodes (note: app.context.typeDisplay contains the data from config.yaml)
+           app.context.labels['sentence'] = ('{rule}', ['rule'])
            # Each key-value pair in dictionary OptionDict represents a specific setting or option for the wg-view.
-           OptionDict = {'hiddenTypes' : 'clause,phrase,subphrase', 'condensed': {True}, 'queryFeatures': {False},'suppress': 'book'}
+           OptionDict = {'hiddenTypes' : 'clause,phrase,subphrase', 'condensed': {True}, 'queryFeatures': {False}, 'suppress' : {''}}
            # Pass the dictionary (with a variable number of pairs) to the displaySetup function to unpack and apply.
            displaySetup(app,**OptionDict)
-           print ('setup for wg-view')
+           feedback+='[wg-view](https://github.com/saulocantanhede/tfgreek2/blob/main/docs/wg-view.md#start)'
 
         if viewName=='syntax':
+           # modify some labels for sentence nodes
+           app.context.labels['sentence'] = ('{function}', ['function'])
            # Each key-value pair in dictionary OptionDict represents a specific setting or option for the syntax-view.
-           OptionDict = {'hiddenTypes' : 'wg', 'condensed': {True}, 'queryFeatures': {False}, 'suppress': 'verse'}
-           # Pass the dictionary (with a variable number of pairs) to the displaySetup function to unpack and apply.
+           OptionDict = {'hiddenTypes' : 'wg, subphrase', 'condensed': {True}, 'queryFeatures': {False},  'suppress' : {''}}
+           # Pass a dictionary (with a variable number of pairs) to the displaySetup function to unpack and apply.
            displaySetup(app,**OptionDict)
-           print ('setup for syntax-view')
+           feedback+='[syntax-view](https://github.com/saulocantanhede/tfgreek2/blob/main/docs/syntax-view.md#start)'
+        app.dm(feedback)
+
+    def __init__(app, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        app.viewtype('syntax')
+        app.dm('See [here](https://github.com/saulocantanhede/tfgreek2/blob/main/docs/viewtypes.md#start) for more information on viewtypes')
+
+
