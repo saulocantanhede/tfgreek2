2fa20d2297d7e9cc53a61831fae581b657ef7d6f
Saulo de Oliveira Cantanhêde <121267518+saulocantanhede@users.noreply.github.com>
2024-07-04 13:41:16 -0500

    Update in sentences, clauses and features

---
+++ tf/0.5.9/otext.tf
@@ -0,0 +1,26 @@
+@config
+@author=Evangelists and apostles
+@converterSourceLocation=https://github.com/saulocantanhede/tfgreek2/tree/main/programs
+@converterVersion=0.5.9 (July 3, 2024)
+@converters=Saulo de Oliveira Cantanhêde, Tony Jurg, Dirk Roorda
+@dataSource=MACULA Greek Linguistic Datasets, available at https://github.com/Clear-Bible/macula-greek/tree/main/Nestle1904/lowfat
+@dataSourceFormat=XML lowfat tree data
+@dataSourceLocation=https://github.com/saulocantanhede/tfgreek2/tree/main/xml/2022-11-01/gnt
+@editors=Eberhart Nestle (1904)
+@fmt:lex-orig-plain={lemma}{punctuation}
+@fmt:lex-translit-plain={lemmatranslit}{punctuation}
+@fmt:text-orig-full={before}{text}{after}
+@fmt:text-orig-plain={text}{punctuation}
+@fmt:text-translit-plain={translit}{punctuation}
+@fmt:text-unaccent-plain={unaccent}{punctuation}
+@institute=ETCBC (Eep Talstra Centre for Bible and Computer) at Vrije Universiteit Amsterdam, CBLC (Center of Biblical Languages and Computing) at Andrews University
+@levelConstraints=clause < group
+@sectionFeatures=book,chapter,verse
+@sectionTypes=book,chapter,verse
+@title=Greek New Testament (Nestle 1904)
+@version=0.5.9
+@xmlSourceDate=February 9, 2023
+@xmlVersion=2022-11-01
+@writtenBy=Text-Fabric
+@dateWritten=2024-07-03T15:42:01Z
+
